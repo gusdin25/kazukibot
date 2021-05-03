22543142091a6e9a1ce924b425858d9c05f51068
gusdin25 <76903567+gusdin25@users.noreply.github.com>
2021-05-03 18:36:27 +0800

    Add files via upload

---
+++ src/cara.js
@@ -0,0 +1,40 @@
+const cara = (pushname, prefix, botName, ownerName) => { 
+	return `🔰 -----[ *PANDUAN PENGGUNAAN ${botName}* ]----- 🔰
+Hallo, ${pushname} 👋
+Berikut adalah cara penggunaan *${botName}*
+         ────────────────
+Kalo gak paham tuh baca dulu -_-
+         ────────────────
+➸ *${prefix}sticker*
+Kirim gambar/video dengan caption sticker
+➸ *${prefix}ttp*
+Text to sticker contoh : ${prefix}ttp Lann ID
+➸ *${prefix}tts*
+Suara google contoh : ${prefix}tts id Hallo Kazuki
+➸ *${prefix}toimg*
+Reply sticker yang mau dijadiin image
+➸ *${prefix}stalkig*
+Stalking instagram contoh : ${prefix}stalkig gusdin_
+➸ *${prefix}quotes*
+Random quotes
+➸ *${prefix}bikinquote*
+Membuat quotes contoh : ${prefix}bikinquote Kamu gay & kazuki
+➸ *${prefix}play*
+Mendownload lagu dari youtube bisa menggunakan text atau link
+➸ *${prefix}tiktod*
+Mendownload video tiktok, gunakan link kak
+➸ *${prefix}pornhub*
+Membuat logo, contoh : ${prefix}pornhub kazuki & Hub
+Note : untuk logo maker / image maker jika undefined berarti harus memakai &
+➸ *${prefix}fitnah*
+Only grup untuk fitnah orang, contoh : ${prefix}fitnah @tagtarget & Hai & Hai juga
+➸ *${prefix}mutual*
+Gacha nomor yang ada di database / yang menggunakan bot
+         ────────────────
+Note : Sisanya gunain otak kalian aja cape w jelasin :)
+         ────────────────
+🔰 -----[ *TUMTOR BY GUSDIN* ]----- 🔰
+`
+}
+
+exports.cara = cara
